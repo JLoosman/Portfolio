ab45d57c6265525a0553ee0f2a70b37278007aa5
Jesiah Loosman <Jesiah.Loosman@outlook.de>
2024-08-17 23:03:54 +0200

    added css animation to make points flimmer like stars

--- src/routes/Point.svelte
+++ src/routes/Point.svelte
@@ -1,9 +1,17 @@
 <script>
   let size = Math.floor(Math.random() * 1.5) + 1;
+  let animationTime = Math.random() * 5 + 5;
+
+  setInterval(() => {
+    size = Math.floor(Math.random() * 1.5) + 1;
+  }, 1000000);
 </script>
 
 <div class="box">
-  <div class="block" style="--heightDot: {size}px; --widthDot: {size}px"></div>
+  <div
+    class="block"
+    style="--heightDot: {size}px; --widthDot: {size}px; --time: {animationTime}s;"
+  ></div>
 </div>
 
 <style>
@@ -13,9 +21,31 @@
   }
   .block {
     margin: 15px;
-    background-color: #eee;
+    background: rgba(255, 255, 255, 0);
     height: var(--heightDot);
     width: var(--widthDot);
     border-radius: 5px;
+    animation: twinkle var(--time) linear var(--time) infinite;
+  }
+
+  @keyframes twinkle {
+    0% {
+      transform: scale(1, 1);
+      background: rgba(255, 255, 255, 0);
+      animation-timing-function: ease-in;
+    }
+    20% {
+      transform: scale(0.8, 0.8);
+      background: rgba(255, 255, 255, 1);
+      animation-timing-function: ease-out;
+    }
+    40% {
+      background: rgba(255, 255, 255, 0);
+      transform: scale(1, 1);
+    }
+    100% {
+      background: rgba(255, 255, 255, 0);
+      transform: scale(1, 1);
+    }
   }
 </style>
