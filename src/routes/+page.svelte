7ce07bb2e0d9e222fb9e63c70cfcf5219a877fd4
Jesiah Loosman <Jesiah.Loosman@outlook.de>
2024-08-17 23:24:33 +0200

    added typing animation to type out name on hero section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,15 @@
 <script>
+  import typer from "typer-js";
+  import { onMount } from "svelte";
   import Point from "./Point.svelte";
+
+  onMount(() => {
+    typer(".text h1", { min: 20, max: 350 }).line("Jesiah Loosman");
+  });
 </script>
 
 <div class="text">
-  <h1>Jesiah Loosman</h1>
+  <h1></h1>
 </div>
 <div class="overlay"></div>
 <div class="grid">
@@ -11,6 +17,14 @@
     <Point />
   {/each}
 </div>
+
+<section>
+  <p>
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero praesentium
+    rerum deserunt vero ducimus alias animi, iste totam esse similique.
+    Quibusdam eligendi incidunt placeat eum libero ab voluptatibus, quas non.
+  </p>
+</section>
 
 <style>
   .grid {
@@ -44,8 +58,10 @@
   }
 
   .text h1 {
+    user-select: none;
     color: #eee;
     font-size: 120px;
+    font-family: Aldrich, sans-serif;
     z-index: 1;
   }
 </style>
